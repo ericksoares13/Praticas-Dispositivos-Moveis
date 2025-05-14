9616f06f506264be6c6eefb76960d37eaa442e17
Erick Soares <erick.s.souza@ufv.br>
2025-05-14 18:08:35 -0300

    Finalizando localização atual

--- app/src/main/java/com/example/mobile/projects/geolocation/GeolocationView.java
+++ app/src/main/java/com/example/mobile/projects/geolocation/GeolocationView.java
@@ -1,37 +1,121 @@
 package com.example.mobile.projects.geolocation;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.mobile.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GeolocationView extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap googleMap;
-    ViewType viewType;
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "requestingLocationUpdates";
+    private static final int FINE_PERMISSION_CODE = 1;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private boolean requestingLocationUpdates;
+
+    private Location currentLocation;
+    private Marker currentMaker;
+
+    private GoogleMap googleMap;
+    private ViewType viewType;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.geolocation_view);
+        this.setContentView(R.layout.geolocation_view);
 
-        this.viewType = getIntent().getSerializableExtra("view", ViewType.class);
-
-        final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        this.viewType = this.getIntent().getSerializableExtra("view", ViewType.class);
+        final SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager().findFragmentById(R.id.map);
 
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
+        this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        this.locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+                .setMinUpdateIntervalMillis(5000)
+                .build();
+        this.locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull final LocationResult locationResult) {
+                if (locationResult.getLastLocation() == null) return;
+                GeolocationView.this.currentLocation = locationResult.getLastLocation();
+            }
+        };
+
+        this.requestingLocationUpdates = false;
+        this.updateValuesFromBundle(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.requestingLocationUpdates) {
+            this.startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.fusedLocationProviderClient.removeLocationUpdates(this.locationCallback);
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                this.requestingLocationUpdates);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void updateValuesFromBundle(final Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            return;
+        }
+
+        if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+            this.requestingLocationUpdates = savedInstanceState.getBoolean(
+                    REQUESTING_LOCATION_UPDATES_KEY);
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+
+        this.requestingLocationUpdates = true;
+        this.fusedLocationProviderClient.requestLocationUpdates(this.locationRequest,
+                this.locationCallback,
+                Looper.getMainLooper());
     }
 
     @Override
@@ -64,8 +148,33 @@ public class GeolocationView extends AppCompatActivity implements OnMapReadyCall
             message = this.viewType.getDescription();
         }
 
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(this.viewType.getLocale(), 15));
-        Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
+        this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(this.viewType.getLocale(), 15));
+        Toast.makeText(this.getBaseContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void selectMyLocale(final View view) {
+        if (!this.requestingLocationUpdates) {
+            this.startLocationUpdates();
+            return;
+        }
+
+        if (this.currentLocation != null) {
+            if (this.currentMaker != null) {
+                this.currentMaker.remove();
+                this.currentMaker = null;
+            }
+
+            final LatLng latLng = new LatLng(this.currentLocation.getLatitude(), this.currentLocation.getLongitude());
+            final MarkerOptions markerOptions = new MarkerOptions().position(latLng)
+                    .title("Localização atual")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+            this.currentMaker = this.googleMap.addMarker(markerOptions);
+
+            this.googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            Toast.makeText(this.getBaseContext(), "Localização atual", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this.getBaseContext(), "Localização ainda não disponível", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public enum ViewType {
